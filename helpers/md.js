575483d7f0145e7590588a191469f7a9dbd4d1bb
Gaël Motte <gaelmotte@Gaels-MacBook-Pro.local>
2024-03-10 08:58:12 +0100

    remove dependency on docker build path

--- helpers/md.js
+++ helpers/md.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
 module.exports = (album, breadcrumbs, options) => {
-    const mdFilePath = album.home ? '/work/albums/text.md' :
-        breadcrumbs.length == 1 ? `/work/albums/${album.title}/text.md` :
-            `/work/albums/${breadcrumbs.slice(1).map(album => album.title).join("/")}/${album.title}/text.md`
+    const mdFilePath = album.home ? './albums/text.md' :
+        breadcrumbs.length == 1 ? `./albums/${album.title}/text.md` :
+            `./albums/${breadcrumbs.slice(1).map(album => album.title).join("/")}/${album.title}/text.md`
     try {
         var md = fs.readFileSync(mdFilePath,
             { encoding: 'utf8', flag: 'r' });
